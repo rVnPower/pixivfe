a0cc4ab7e7b285129bf672321109f973a134c6a2
iacore <iacore@noreply.codeberg.org>
2024-08-28 13:58:12 +0000

    log to stderr with better format

--- audit/log_reporter.go
+++ audit/log_reporter.go
@@ -2,6 +2,8 @@ package audit
 
 import (
 	"log"
+	"os"
+	"time"
 
 	"github.com/openzipkin/zipkin-go/model"
 )
@@ -11,9 +13,13 @@ type LogReporter struct {
 }
 
 func (rr LogReporter) Send(m model.SpanModel) {
-	rr.l.Print(m.Name)
+	rr.l.Printf("%s | %v +%.3fs", m.Name,  m.Timestamp.Format(time.RFC3339), float64(m.Duration) / float64(time.Second) )
 }
 
 func (rr LogReporter) Close() error {
 	return nil
+}
+
+func NewLogReporter() LogReporter {
+	return LogReporter{l: log.New(os.Stderr, "", 0)}
 }
