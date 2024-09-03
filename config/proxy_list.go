1ae1995655095a23a88de949ccd1016bdd59a43b
VnPower <vnpower@disroot.org>
2024-09-03 17:02:50 +0700

    Update list of image proxies

--- config/proxy_list.go
+++ config/proxy_list.go
@@ -8,9 +8,15 @@ var BuiltinProxyList = []string{
 	"https://pximg.exozy.me", // except this one. this one we are affiliated with.
 	"https://pixiv.ducks.party",
 	"https://pximg.cocomi.eu.org",
-	// "https://mima.localghost.org/proxy/pximg", // doesn't support HTTP/1.1. only support HTTP/2. need proxy code to use a http client (not http.Client) that supports HTTP/2.
+	"https://i.suimoe.com",
+	"https://i.yuki.sh",
+	"https://pximg.obfs.dev",
 	"https://pixiv.darkness.services",
+	"https://pixiv.tatakai.top",
+	"https://pi.169889.xyz",
 	"https://i.pixiv.re",
-	// "https://pixiv.tatakai.top", // dead due to us :(
+	// "https://mima.localghost.org/proxy/pximg", // doesn't support HTTP/1.1. only support HTTP/2. need proxy code to use a http client (not http.Client) that supports HTTP/2.
 	// "https://pximg.chaotic.ninja", // incompatible
+
+	// VnPower: Please comment non-working sites instead of deleting them.
 }
