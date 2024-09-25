e7e3bfffd89fbc86987c6bb59899e8543b57fc6e
perennial <mail@perennialte.ch>
2024-09-25 22:45:37 +1000

    Convert nav + about page + settings page draft
    Also disable CSP

--- server/middleware/csp.go
+++ server/middleware/csp.go
@@ -1,11 +1,7 @@
 package middleware
 
 import (
-	"fmt"
 	"net/http"
-	"strings"
-
-	"codeberg.org/vnpower/pixivfe/v2/server/session"
 )
 
 // SetPrivacyHeaders is a middleware that adds security headers to HTTP responses.
@@ -19,9 +15,6 @@ func SetPrivacyHeaders(h http.Handler) http.Handler {
 		header.Add("Strict-Transport-Security", "max-age=31536000; includeSubDomains; preload")
 		// use this if need iframe: `frame-ancestors 'self'`
 		header.Add("Permissions-Policy", "accelerometer=(), ambient-light-sensor=(), battery=(), camera=(), display-capture=(), document-domain=(), encrypted-media=(), execution-while-not-rendered=(), execution-while-out-of-viewport=(), geolocation=(), gyroscope=(), magnetometer=(), microphone=(), midi=(), navigation-override=(), payment=(), publickey-credentials-get=(), screen-wake-lock=(), sync-xhr=(), usb=(), web-share=(), xr-spatial-tracking=()")
-		if !strings.HasPrefix(r.URL.Path, "/diagnostics") {
-			header.Add("Content-Security-Policy", fmt.Sprintf("base-uri 'self'; default-src 'none'; script-src 'self'; style-src 'self'; img-src 'self' %s; media-src 'self' %s; connect-src 'self'; form-action 'self'; frame-ancestors 'none';", session.GetImageProxyOrigin(r), session.GetImageProxyOrigin(r)))
-		}
 
 		h.ServeHTTP(w, r)
 	})
