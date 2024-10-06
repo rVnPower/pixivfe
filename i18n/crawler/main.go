6ec01c48a37a08ae0edf2763aba7213981c0d0aa
iacore <iacore@noreply.codeberg.org>
2024-10-06 08:37:00 +0000

    [i18n] fix parser
    removed some false positives

--- i18n/crawler/main.go
+++ i18n/crawler/main.go
@@ -8,7 +8,7 @@ import (
 	"github.com/yargevad/filepathx"
 )
 
-var re_command = regexp.MustCompile(`\A([\p{Zs}\n]*\{\{[^\{\}]*\}\})*[\p{Zs}\n]*\z`)
+var re_command = regexp.MustCompile(`\A([\s\n]*\{\{[^\{\}]*\}\})*[\s\n]*\z`)
 var re_comment = regexp.MustCompile(`\{\*[\s\S]*?\*\}`)
 
 func stripComments(s string) string {
