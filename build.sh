ec488a4882b4aff973bd3ea47556215cd67865e5
iacore <iacore@noreply.codeberg.org>
2024-10-06 08:20:49 +0000

    Use jq to display template_strings.json

--- build.sh
+++ build.sh
@@ -30,7 +30,7 @@ test() {
 
 i18n() {
     semgrep scan -q -f i18n/semgrep-i18n.yml --json | jq '.results | map({msg:.extra.metavars["$MSG"].abstract_content, file:.path, line:.start.line, offset:.start.offset})' > i18n/error_strings.json
-    go run ./i18n/crawler > i18n/template_strings.json
+    go run ./i18n/crawler | jq > i18n/template_strings.json
     echo "Malformed:"
     jq '.[] | select(.msg | contains("\n"))' < i18n/template_strings.json
 }
