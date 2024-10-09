480492b40c6d04dad879374ef820de06e2b14a21
VnPower <vnpower@disroot.org>
2024-10-09 20:06:14 +0700

    i18n: add exceptions

--- i18n/exceptions.go
+++ i18n/exceptions.go
@@ -3,4 +3,8 @@ package i18n
 var IgnoreTheseStrings = map[string]bool {
 	"": true,
 	"»": true,
+	"▶": true,
+	"⧉ {{ .Pages }}": true,
+        "PixivFE": true,
+        "pixiv.net/i/{{ .ID }}": true,
 }
